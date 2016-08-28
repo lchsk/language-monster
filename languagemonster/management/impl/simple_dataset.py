3b885a728e7ee103b72f5bdba15f030d8f6341b1
lchsk <mjlechowski@gmail.com>
2016-08-28 13:54:01 +0100

    Fix create dataset from

--- languagemonster/management/impl/simple_dataset.py
+++ languagemonster/management/impl/simple_dataset.py
@@ -13,59 +13,6 @@ from core.models import *
 logger = logging.getLogger(__name__)
 settings.LOGGER(logger, settings.LOG_WORKERS_HANDLER)
 
-
-@login_required
-@staff_member_required
-def simple_dataset_from(request, dataset_id):
-    c = get_context(request)
-
-    c['simple_datasets'] = SimpleDataset.objects.all()
-    c['dataset_id'] = dataset_id
-
-    return render(request, "app/management/form_simple_dataset_from.html", c)
-
-@login_required
-@staff_member_required
-def generate_simple_dataset(request, dataset_id):
-    c = get_context(request)
-
-    ds = DataSet.objects(pk=dataset_id).first()
-    simple_dataset_id = request.POST['simple_dataset_id']
-    title = request.POST['title']
-    base_or_target = request.POST['base_or_target']
-    sds = SimpleDataset.objects(pk=simple_dataset_id).first()
-
-    if ds and sds and base_or_target in ('base', 'target') and title:
-        simple_def = sds.data.split('\n')
-        simple_def = [ x.strip() for x in simple_def if x ]
-
-        new_ds = DataSet(
-            icon=ds.icon,
-            name_base=ds.name_base,
-            name_en=title,
-            name_target=ds.name_target,
-            pair=ds.pair,
-            visible=False,
-            word_count=0,
-            pos=ds.pos,
-            from_exported_file=ds.from_exported_file,
-            simple_dataset=True
-        )
-        new_ds.save()
-
-        wp_tmp = DS2WP.objects(ds=ds)
-        word_pairs = [ x.wp for x in wp_tmp ]
-
-        for wp in word_pairs:
-            if getattr(wp, base_or_target, None) in simple_def:
-                new_link = DS2WP(
-                    ds = new_ds,
-                    wp = wp
-                )
-                new_link.save()
-
-    return redirect(reverse('management:index'))
-
 @login_required
 @staff_member_required
 def view_copy_words_to(request, target_dataset_id):
