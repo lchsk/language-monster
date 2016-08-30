cb16d7984da07c631277b3c4db02692b16094ca8
lchsk <mjlechowski@gmail.com>
2016-08-30 19:18:09 +0100

    Comment out ui selenium tests

--- languagemonster/core/tests/ui/test_auth.py
+++ languagemonster/core/tests/ui/test_auth.py
@@ -1,88 +1,88 @@
-import random
-import unittest
+# import random
+# import unittest
 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.common.exceptions import NoSuchElementException
 
-from util import when_visible
+# from util import when_visible
 
-URL = 'http://localhost:8000'
-TEST_EMAIL = ''
-TEST_PASSWD = ''
+# URL = 'http://localhost:8000'
+# TEST_EMAIL = ''
+# TEST_PASSWD = ''
 
-def login(driver):
-    driver.get(URL)
+# def login(driver):
+#     driver.get(URL)
 
-    assert 'Language Monster' in driver.title
+#     assert 'Language Monster' in driver.title
 
-    login_link = driver.find_element_by_id('login')
-    login_link.click()
+#     login_link = driver.find_element_by_id('login')
+#     login_link.click()
 
-    # login_popup = WebDriverWait(driver, 10).until(
-    #     EC.visibility_of_element_located((By.ID, "modal-login"))
-    # )
+#     # login_popup = WebDriverWait(driver, 10).until(
+#     #     EC.visibility_of_element_located((By.ID, "modal-login"))
+#     # )
 
-    login_popup = when_visible(driver, ID='modal-login')
+#     login_popup = when_visible(driver, ID='modal-login')
 
-    input_email = login_popup.find_element_by_id('modal-login-email')
-    input_email.send_keys(TEST_EMAIL)
+#     input_email = login_popup.find_element_by_id('modal-login-email')
+#     input_email.send_keys(TEST_EMAIL)
 
-    input_passwd = login_popup.find_element_by_id('modal-login-password')
-    input_passwd.send_keys(TEST_PASSWD)
+#     input_passwd = login_popup.find_element_by_id('modal-login-password')
+#     input_passwd.send_keys(TEST_PASSWD)
 
-    btn_submit = login_popup.find_element_by_class_name('btn-submit')
-    btn_submit.click()
+#     btn_submit = login_popup.find_element_by_class_name('btn-submit')
+#     btn_submit.click()
 
-    login_success = False
+#     login_success = False
 
-    try:
-        hello_str = driver.find_element_by_class_name('hello-screen')
-        login_success = True
-    except NoSuchElementException as e:
-        login_success = False
-        print e
+#     try:
+#         hello_str = driver.find_element_by_class_name('hello-screen')
+#         login_success = True
+#     except NoSuchElementException as e:
+#         login_success = False
+#         print e
 
-    return login_success
+#     return login_success
 
-class TestAuth(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Firefox()
+# class TestAuth(unittest.TestCase):
+#     def setUp(self):
+#         self.driver = webdriver.Firefox()
 
-    def tearDown(self):
-        self.driver.close()
+#     def tearDown(self):
+#         self.driver.close()
 
-    def test_login(self):
-        self.assertTrue(login(self.driver), 'Login failed')
+#     def test_login(self):
+#         self.assertTrue(login(self.driver), 'Login failed')
 
 
-# 
+# #
 
-# 
-#     play_btns = driver.find_elements_by_class_name('learn')
-#     random.choice(play_btns).click()
-# 
-#     elem = driver.find_element_by_id('btn-all')
-#     elem.click()
-# 
-#     elem = driver.find_element_by_id('all')
-# 
-#     dataset_btns = elem.find_elements_by_class_name('play')
-#     random.choice(dataset_btns).click()
-# 
-#     WebDriverWait(driver, 10).until(
-#         lambda driver: driver.execute_script('return window.game.to_ask != undefined;')
-#     )
-# 
-#     print driver.execute_script('return window.game.to_ask.length;')
-# 
-# 
-# 
+# #
+# #     play_btns = driver.find_elements_by_class_name('learn')
+# #     random.choice(play_btns).click()
+# #
+# #     elem = driver.find_element_by_id('btn-all')
+# #     elem.click()
+# #
+# #     elem = driver.find_element_by_id('all')
+# #
+# #     dataset_btns = elem.find_elements_by_class_name('play')
+# #     random.choice(dataset_btns).click()
+# #
+# #     WebDriverWait(driver, 10).until(
+# #         lambda driver: driver.execute_script('return window.game.to_ask != undefined;')
+# #     )
+# #
+# #     print driver.execute_script('return window.game.to_ask.length;')
+# #
+# #
+# #
 
-# 
+# #
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
--- languagemonster/core/tests/ui/util.py
+++ languagemonster/core/tests/ui/util.py
@@ -1,31 +1,31 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
 
-# Seconds
-WAITING_TIME = 10
+# # Seconds
+# WAITING_TIME = 10
 
-def when_visible(driver, ID=None, CLASS_NAME=None):
-    condition = None
+# def when_visible(driver, ID=None, CLASS_NAME=None):
+#     condition = None
 
-    if ID is not None:
-        condition = By.ID, ID
-    elif CLASS_NAME is not None:
-        condition = By.CLASS_NAME, CLASS_NAME
+#     if ID is not None:
+#         condition = By.ID, ID
+#     elif CLASS_NAME is not None:
+#         condition = By.CLASS_NAME, CLASS_NAME
 
-    if condition is not None:
-        return WebDriverWait(driver, WAITING_TIME).until(
-            EC.visibility_of_element_located(condition)
-        )
+#     if condition is not None:
+#         return WebDriverWait(driver, WAITING_TIME).until(
+#             EC.visibility_of_element_located(condition)
+#         )
 
-def when_condition(driver, condition):
-    return WebDriverWait(driver, WAITING_TIME).until(
-        condition
-    )
+# def when_condition(driver, condition):
+#     return WebDriverWait(driver, WAITING_TIME).until(
+#         condition
+#     )
 
-def reset_driver(driver):
-    if driver:
-        driver.close()
+# def reset_driver(driver):
+#     if driver:
+#         driver.close()
 
-    return webdriver.Firefox()
+#     return webdriver.Firefox()
