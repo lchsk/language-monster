b866afdeca803caa9c498c0379bfe49e9d2eb659
lchsk <mjlechowski@gmail.com>
2016-09-03 19:25:01 +0100

    Create userprofile views dir

--- languagemonster/languagemonster/urls.py
+++ languagemonster/languagemonster/urls.py
@@ -7,7 +7,7 @@ from core.views import (
     ErrorPage,
 )
 from django.conf.urls.static import static
-import userprofile.views as userprofile
+from userprofile.views.social import UserPage
 
 handler400 = ErrorPage.as_view(error=400)
 handler404 = ErrorPage.as_view(error=404)
@@ -24,7 +24,7 @@ urlpatterns = [
 
     url(
         r'^profile/(?P<identifier>.+?)/?$',
-        userprofile.UserPage.as_view(),
+        UserPage.as_view(),
         name='public_page',
     ),
     url(r'^manage/', include('management.urls', namespace='management')),
