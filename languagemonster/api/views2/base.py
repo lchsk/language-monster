b8f47358c62db62c70b11a9ad06ec5fd90ef5644
lchsk <mjlechowski@gmail.com>
2016-09-05 20:16:08 +0100

    Add MonsterUserAuthView

--- languagemonster/api/views2/base.py
+++ languagemonster/api/views2/base.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 
+from core.models import MonsterUser
+
 from rest_framework.authentication import BaseAuthentication
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.views import APIView
@@ -20,5 +22,19 @@ class APIAuth(BaseAPIAuth):
         if self._token != settings.API_KEY:
             raise AuthenticationFailed('Unauthorised')
 
-class APIAuthView(APIAuth, APIView):
+class APIAuthView(APIView):
     authentication_classes = (APIAuth,)
+
+class MonsterUserAuth(BaseAPIAuth):
+    def authenticate(self, request):
+        super(MonsterUserAuth, self).authenticate(request)
+
+        monster_user = MonsterUser.objects.filter(
+            api_login_hash=self._token
+        )
+
+        if len(monster_user) != 1:
+            raise AuthenticationFailed('Unauthorised')
+
+class MonsterUserAuthView(APIView):
+    authentication_classes = (MonsterUserAuth,)
