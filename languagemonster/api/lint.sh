e43df39bce3ecb1e6f2573d01d93122def99612b
lchsk <mjlechowski@gmail.com>
2016-09-26 21:32:18 +0100

    Remove lint.sh files

--- languagemonster/api/lint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-
-pylint --load-plugins pylint_django ../api
--- languagemonster/lint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd ./api && ./lint.sh
-cd ./core && ./lint.sh
-cd ./ctasks && ./lint.sh
-cd ./management && ./lint.sh
-cd ./userprofile && ./lint.sh
-cd ./utility && ./lint.sh
-cd ./vocabulary && ./lint.sh
