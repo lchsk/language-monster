7f43a4096627e92e16c1751d790170211bf26dd5
lchsk <mjlechowski@gmail.com>
2016-11-05 20:11:25 +0000

    Configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
 module.exports = {
-    "extends": "eslint:recommended"
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": ["error", 4]
+    },
+    "env": {
+        "browser": true
+    },
+    "globals": {
+        "MONSTER": true,
+        "PIXI": true
+    }
 };
