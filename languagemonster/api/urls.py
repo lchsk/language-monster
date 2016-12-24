6953ec6c104953a303c8e9f5c2564d9fd1b3da71
lchsk <mjlechowski@gmail.com>
2016-12-24 19:34:06 +0000

    Change local words endpoint regex

--- languagemonster/api/urls.py
+++ languagemonster/api/urls.py
@@ -61,7 +61,7 @@ urlpatterns = [
         name='words',
     ),
     url(
-        r'^local/words/(?P<dataset_id>\d+)/(?P<uri>.+)?/?$',
+        r'^local/words/(?P<dataset_id>\d+)(/(?P<uri>.+))?/?$',
         LocalGetWords.as_view(),
         name='local_words',
     ),
