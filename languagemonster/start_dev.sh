e005faa064f1acb93dbc6688cc2a81582e370dbb
lchsk <mjlechowski@gmail.com>
2017-05-28 18:05:51 +0100

    Run all required apps in start_dev.sh

--- languagemonster/start_dev.sh
+++ languagemonster/start_dev.sh
@@ -3,5 +3,9 @@
 . ../bin/activate
 export $(cat ../../language-monster-deploy/dev_env | xargs)
 
+# postgres
 redis-server &
+gulp watch &
+celery -A languagemonster beat &
+celery -A languagemonster worker &
 python manage.py runserver 8000
